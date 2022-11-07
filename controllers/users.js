d2c931b1a967ae0e4a3ab1490f13e13a82375e29
Kurtney Joseph <josephkurt18@gmail.com>
2022-11-06 21:02:22 -0500

    add editorconfig file

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
-const User = require('../models/user');
+const mongoose = require("mongoose");
+const User = require("../models/user");
+const { isValidUrl } = require("../utils/url");
 
 module.exports.getUser = (req, res) => {
   if (mongoose.Types.ObjectId.isValid(req.params.userId) === false) {
-    return res.status(400).send({ message: 'Not valid user ID' });
+    return res.status(400).send({ message: "Not valid user ID" });
   }
   User.findById(req.params.userId)
     .orFail()
@@ -11,10 +12,10 @@ module.exports.getUser = (req, res) => {
     .catch((err) => {
       console.log(err);
       const ERROR_CODE = 404;
-      if (err.name === 'DocumentNotFoundError') {
+      if (err.name === "DocumentNotFoundError") {
         return res
           .status(ERROR_CODE)
-          .send({ message: 'Requested resource not found' });
+          .send({ message: "Requested resource not found" });
       }
     });
 };
@@ -24,25 +25,28 @@ module.exports.getUsers = (req, res) => {
     .then((users) => res.status(200).send([{ data: users }]))
     .catch((err) => {
       const ERROR_CODE = 500;
-      if (err.name === 'InternalServerError') {
+      if (err.name === "InternalServerError") {
         return res
           .status(ERROR_CODE)
-          .send({ message: 'An error has occured on the server' });
+          .send({ message: "An error has occured on the server" });
       }
     });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, avatar } = req.body;
-
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send({ data: user }))
-    .catch((err) => {
-      const ERROR_CODE = 400;
-      if (err.name === 'ValidationError') {
-        return res
-          .status(ERROR_CODE)
-          .send({ message: 'User validation failed' });
-      }
-    });
+  if (!isValidUrl(avatar)) {
+    return res.status(400).send({ message: `Not a valid URL` });
+  } else {
+    User.create({ name, avatar })
+      .then((user) => res.status(201).send({ data: user }))
+      .catch((err) => {
+        const ERROR_CODE = 400;
+        if (err.name === "ValidationError") {
+          return res
+            .status(ERROR_CODE)
+            .send({ message: "User validation failed" });
+        }
+      });
+  }
 };
