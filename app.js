c6ecdeec3f23149c0f746af604eb6d8ff9447b58
Kurtney Joseph <josephkurt18@gmail.com>
2022-11-07 12:34:53 -0500

    modify files

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const router = require("./routes");
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes');
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect("mongodb://localhost:27017/wtwr_db");
+mongoose.connect('mongodb://localhost:27017/wtwr_db');
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "63659e74a3429fb348b86dbe",
+    _id: '63659e74a3429fb348b86dbe',
   };
   next();
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", router);
+app.use('/', router);
 app.use((req, res, next) => {
   const statusCode = 404;
 
-  const message = `Requested resource not found`;
+  const message = 'Requested resource not found';
 
   res.status(statusCode).send({ message });
   next();
--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,20 @@
-const mongoose = require("mongoose");
-const User = require("../models/user");
-const { isValidUrl } = require("../utils/url");
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const { isValidUrl } = require('../utils/url');
 
 module.exports.getUser = (req, res) => {
   if (mongoose.Types.ObjectId.isValid(req.params.userId) === false) {
-    return res.status(400).send({ message: "Not valid user ID" });
+    return res.status(400).send({ message: 'Not valid user ID' });
   }
   User.findById(req.params.userId)
     .orFail()
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
-      console.log(err);
       const ERROR_CODE = 404;
-      if (err.name === "DocumentNotFoundError") {
+      if (err.name === 'DocumentNotFoundError') {
         return res
           .status(ERROR_CODE)
-          .send({ message: "Requested resource not found" });
+          .send({ message: 'Requested resource not found' });
       }
     });
 };
@@ -25,10 +24,10 @@ module.exports.getUsers = (req, res) => {
     .then((users) => res.status(200).send([{ data: users }]))
     .catch((err) => {
       const ERROR_CODE = 500;
-      if (err.name === "InternalServerError") {
+      if (err.name === 'InternalServerError') {
         return res
           .status(ERROR_CODE)
-          .send({ message: "An error has occured on the server" });
+          .send({ message: 'An error has occured on the server' });
       }
     });
 };
@@ -36,17 +35,16 @@ module.exports.getUsers = (req, res) => {
 module.exports.createUser = (req, res) => {
   const { name, avatar } = req.body;
   if (!isValidUrl(avatar)) {
-    return res.status(400).send({ message: `Not a valid URL` });
-  } else {
-    User.create({ name, avatar })
-      .then((user) => res.status(201).send({ data: user }))
-      .catch((err) => {
-        const ERROR_CODE = 400;
-        if (err.name === "ValidationError") {
-          return res
-            .status(ERROR_CODE)
-            .send({ message: "User validation failed" });
-        }
-      });
+    return res.status(400).send({ message: 'Not a valid URL' });
   }
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send({ data: user }))
+    .catch((err) => {
+      const ERROR_CODE = 400;
+      if (err.name === 'ValidationError') {
+        return res
+          .status(ERROR_CODE)
+          .send({ message: 'User validation failed' });
+      }
+    });
 };
