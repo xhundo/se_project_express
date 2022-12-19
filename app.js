9651a68bdb890aa36835c30bbf969d622a698f7a
Kurtney Joseph <josephkurt18@gmail.com>
2022-12-18 19:02:28 -0500

    update cors

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const auth = require('./middlewares/auth');
 const router = require('./routes');
 
 const app = express();
+const corOptions = {
+  origin: 'https://www.wtwr-demo.students.nomoredomainssbs.ru/',
+};
 
 const { PORT = 3000 } = process.env;
 const { errorHandle } = require('./errors/errors');
@@ -17,6 +20,9 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { validateURL } = require('./utils/validator');
 
 mongoose.connect('mongodb://localhost:27017/wtwr_db');
+
+app.use(cors(corOptions));
+app.options('*', cors(corOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
