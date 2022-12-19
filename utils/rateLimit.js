709e8689a9c176f18f1577874c8da8cd80e5cf31
Kurtney Joseph <josephkurt18@gmail.com>
2022-12-19 16:53:15 -0500

    modify rate limit

--- utils/rateLimit.js
+++ utils/rateLimit.js
@@ -1,4 +1,4 @@
-const { rateLimit } = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
