a0d913bc7fd4e6ab22753ad9b93874edb3e10527
Kurtney Joseph <josephkurt18@gmail.com>
2022-12-20 12:19:39 -0500

    update middlewares

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const { errors, celebrate, Joi } = require('celebrate');
@@ -16,7 +16,12 @@ const { errorHandle } = require('./errors/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { validateURL } = require('./utils/validator');
 
-const limiter = require('./utils/rateLimit');
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 app.set('trust proxy');
 
